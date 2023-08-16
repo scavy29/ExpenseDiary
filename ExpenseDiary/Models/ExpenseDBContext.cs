909393c4a1e3c5a12a15e15bd6ceaa252026ce9f
scavy29 <vikrant.dekate1203@gmail.com>
2023-08-16 21:39:05 +0530

    CRUD Done

---
+++ ExpenseDiary/Models/ExpenseDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseDiary.Models
+{
+    public class ExpenseDBContext:DbContext
+    {
+
+    //Constructor with Parameters
+    public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options) : base(options)
+        {}
+
+        /*Table for the model class*/
+        public DbSet<ExpenseReport> Expenses { get; set; }
+    }
+}
+
